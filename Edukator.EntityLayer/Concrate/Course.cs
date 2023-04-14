5cb7ecd36dd3e8e17928b7ae8d0d1e0140daf1d4
Ali Kaan Yayla <naklovcuas@gmail.com>
2023-04-14 17:24:46 +0300

    Add project files.

---
+++ Edukator.EntityLayer/Concrate/Course.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edukator.EntityLayer.Concrate
+{
+    public class Course
+    {
+        public int CourseID { get; set; }
+
+        public string CourseTitle { get; set; }
+
+        public string CourseDescription { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Review { get; set; }
+    }
+}
